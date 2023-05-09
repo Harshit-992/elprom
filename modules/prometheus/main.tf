b9c62a99a3c35a98a371ffc97da5961c4fc42c78
Harshit-992 <85869265+Harshit-992@users.noreply.github.com>
2023-05-09 10:13:25 +0530

    Add files via upload

---
+++ modules/prometheus/main.tf
@@ -0,0 +1,14 @@
+
+resource "aws_instance" "prometheus" {
+  ami           = data.aws_ami.amazon-linux.id
+  instance_type = "t2.medium"
+  vpc_security_group_ids = ["${aws_security_group.prom_sg.id}"]
+  
+  tags = {
+    Name = "prometheus"
+  }
+#    user_data = "${file("init.sh")}"
+    user_data = "${data.template_file.promscript.rendered}"
+
+ #  user_data = file("${path.module}/script.sh ")
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  access_key = "AKIA2NDO4OQES7D6J4MJ"
+  secret_key = "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"
+}
+
